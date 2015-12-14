edd03c39eef83e5f46a0933b5ef881b982ecb1b6
Kyle Sullivan <kyle@kitchensinkmarketing.com>
2015-12-14 18:11:24 -0500

    Checkpoint 3
    Most of the desktop animation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,61 @@
+
+
 $( function(){
     var timelineEventNumber = $( '.timeline-box' ).length;
+    var maxHeight = -1;
+    $( '.timeline-box' ).each( function(){
+        var $this = $( this );
+        var idx = $this.index();
 
-    var timelineEvents = [];
+        var leftPct = idx / ( timelineEventNumber - 1 ) * 100;
+        $this.css({
+            'left'  : leftPct + '%'
+        });
 
-    for ( var i = 1; i <= timelineEventNumber; i++ ) {
+        $this.parent( '.timeline-bar' ).append( '<div class="timeline-event event-' + idx + '" style="left: ' + leftPct + '%;"></div>' );
 
-        var leftPct = ( i - 1) / ( timelineEventNumber - 1 ) * 100;
+        $this.addClass( 'event-' + idx );
 
-        style = ' style="left: ' + leftPct + '%;"';
+        var topBoxWidth = ( 1 / ( Math.round( timelineEventNumber / 2 ) ) * 100 ) + '%';
+        $this.css({
+            'width' : topBoxWidth
+        });
 
+        $this.find( '.inner-timeline-box' ).width( $this.width() );
 
-        timelineEvents.push( '<div class="timeline-event event-' + i  +  '"' + style + ' />' );
+        if( ( idx + 1 ) === timelineEventNumber ){
+            $this.addClass( 'last-event' ).css({
+                'width' : parseFloat( topBoxWidth ) * 0.75 + '%'
+            });
+        }
+
+        maxHeight = maxHeight > $this.outerHeight() ? maxHeight : $this.outerHeight();
+    });
+    //$( '.timeline-box' ).css({ 'min-height' : maxHeight });
+    $( '.timeline-bar' ).css({ 'margin' : ( maxHeight + 80) + 'px 0' });
+
+    var timelineAdvance = setInterval( timer, 400);
+    var idx = 0;
+
+    function timer(){
+        $( '.event-' + idx ).addClass( 'show-box' );
+        idx++;
+        var leftPct = ( idx / ( timelineEventNumber - 1 ) * 100 ) + '%';
+        $( '.timeline-overlay' ).css('width', leftPct );
+        if( idx > timelineEventNumber ){
+            clearInterval( timelineAdvance );
+            $( '.inner-timeline-box' ).removeAttr( 'style' );
+        }
     }
 
-    $( '.timeline-bar' ).append( timelineEvents.join( '\n' ) );
 
+});
 
+$( window ).bind('resize orientationchange', function() {
+    var maxHeight = -1;
+    $( '.timeline-box' ).each( function(){
+        var $this = $( this );
+        maxHeight = maxHeight > $this.outerHeight() ? maxHeight : $this.outerHeight();
+    });
+    $( '.timeline-bar' ).css({ 'margin' : ( maxHeight + 80) + 'px 0' });
 });
