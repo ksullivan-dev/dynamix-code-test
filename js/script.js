3d5840634e1a9713487599c968c39a0617ecd658
Kyle Sullivan <kyle@kitchensinkmarketing.com>
2015-12-15 18:20:52 -0500

    Checkpoint 5
    Lots of MQ styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
-
+var containerClass;
 $( function(){
+
+
+
     var timelineEventNumber = $( '.timeline-box' ).length;
     var maxHeight = -1;
     $( '.timeline-box' ).each( function(){
@@ -16,7 +19,7 @@ $( function(){
 
         $this.addClass( 'event-' + idx );
 
-        var topBoxWidth = ( 1 / ( Math.round( timelineEventNumber / 2 ) ) * 100 ) + '%';
+        var topBoxWidth = ( 1 / ( Math.round( ( timelineEventNumber - 1) / 2 ) ) * 100 ) - 5 + '%';
         $this.css({
             'width' : topBoxWidth
         });
@@ -25,7 +28,7 @@ $( function(){
 
         if( ( idx + 1 ) === timelineEventNumber ){
             $this.addClass( 'last-event' ).css({
-                'width' : parseFloat( topBoxWidth ) * 0.75 + '%'
+                'width' : parseFloat( topBoxWidth ) * 0.7 + '%'
             });
         }
         if( $this.is( ':nth-child(odd)' ) ){
@@ -34,16 +37,20 @@ $( function(){
     });
     //$( '.timeline-box' ).css({ 'min-height' : maxHeight });
     $( '.timeline-bar' ).css({ 'margin' : ( maxHeight + 40 ) + 'px 0' });
+    $( '.timeline-bar' ).prepend( '<div class="timeline-overlay-mobile" />' )
 
-    var timelineAdvance = setInterval( timer, 400);
+    var timelineAdvance = setInterval( timer, 600);
     var idx = 0;
 
     function timer(){
         $( '.event-' + idx ).addClass( 'show-box' );
+        //$( '.desktop' ).find( '.event-' + idx ).addClass( 'show-box' );
+        var topPct = ( idx / ( timelineEventNumber - 1 ) * 100 ) + '%';
         idx++;
         var leftPct = ( idx / ( timelineEventNumber - 1 ) * 100 ) + '%';
         $( '.timeline-overlay' ).css('width', leftPct );
-        if( idx > timelineEventNumber ){
+        $( '.timeline-overlay-mobile' ).css('height', topPct );
+        if( idx === timelineEventNumber ){
             clearInterval( timelineAdvance );
             $( '.inner-timeline-box' ).removeAttr( 'style' );
         }
@@ -56,13 +63,65 @@ $( function(){
         'margin-left' : parseFloat( discoveryStylesStart[ 1 ] ) + '%'
     });
 
+    majorBreakpoint();
+    containerClass = $( '.main-container' ).attr( 'class' );
+    mobileTimeline();
+
 });
 
+
+function majorBreakpoint() {
+	if ( $( '#mobile' ).is( ':visible' ) ) {
+		$( '.main-container' ).addClass( 'mobile' ).removeClass( 'desktop' );
+	}
+    if ( $( '#desktop' ).is( ':visible' ) ) {
+		$( '.main-container' ).addClass( 'desktop' ).removeClass( 'mobile' );
+	}
+}
+
+
+
+
+function mobileTimeline(){
+    var firstMobileTimelineEvent = $( '.timeline-box:first' ).offset().top;
+    var lastMobileTimelineEvent = $( '.timeline-box:last' ).offset().top;
+    var timelineBarHeight =  $( '.timeline-bar' ).height();
+
+    $( '.mobile' ).find( '.timeline-bar' ).css({
+        'height' : lastMobileTimelineEvent - firstMobileTimelineEvent + 'px'
+    });
+
+
+}
+
 $( window ).bind('resize orientationchange', function() {
+
+
+
+
+
     var maxHeight = -1;
     $( '.timeline-box' ).each( function(){
         var $this = $( this );
-        maxHeight = maxHeight > $this.outerHeight() ? maxHeight : $this.outerHeight();
+        if( $this.is( ':nth-child(odd)' ) ){
+            maxHeight = maxHeight > $this.outerHeight() ? maxHeight : $this.outerHeight();
+        }
     });
-    $( '.timeline-bar' ).css({ 'margin' : ( maxHeight + 80) + 'px 0' });
+    $( '.timeline-bar' ).css({ 'margin' : ( maxHeight + 40 ) + 'px 0' });
+
+
+    majorBreakpoint();
+    mobileTimeline();
+
+    var newContainerClass = $( '.main-container' ).attr( 'class' );
+	if ( containerClass != newContainerClass ) {
+        if( newContainerClass.indexOf( 'mobile' ) > -1 ){
+            $( '.timeline-overlay' ).hide();
+        }
+        if( newContainerClass.indexOf( 'desktop' ) > -1 ){
+            $( '.timeline-overlay' ).show();
+            $( '.desktop' ).find( '.timeline-bar' ).css('height' , '4px');
+        }
+	}
+	containerClass = newContainerClass;
 });
